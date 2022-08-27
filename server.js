b4c79b7ef9b163c64f37590ae5ae20db32fa1f05
artemfob <artemfob@gmail.com>
2022-08-27 12:50:51 +0300

    lets try

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const http = require('http')
-http
-    .createServer(function (request, response) {
-        response.end('Hello NodeJS!')
-    })
-    .listen(3000, '127.0.0.1', function () {
-        console.log(
-            'Сервер начал прослушивание запросов на порту 3000'
-        )
-    })
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT||80;
+app.get('/',(req, res)=>{
+    res.end('<h1>server work</h1>')
+})
+
+app.listen(PORT, ()=>{
+
+})
