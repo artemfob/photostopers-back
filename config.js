a7cb4d4dd099d269351108e10ee71c9547f96558
artemfob <artemfob@gmail.com>
2022-08-31 18:25:37 +0300

    connect to firebase

---
+++ config.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path')
+const app = express();
+const firebase = require('firebase')
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC8p-8UeASrWJaSCcfIJl4U5HrRF4fZxjk",
+    authDomain: "photostopers.firebaseapp.com",
+    projectId: "photostopers",
+    storageBucket: "photostopers.appspot.com",
+    messagingSenderId: "68439398764",
+    appId: "1:68439398764:web:3740d8f0a6c516190c3b49"
+};
+module.exports = {express, path, app, firebase, firebaseConfig};
--- server.js
+++ server.js
@@ -1,10 +1,13 @@
-const express = require('express');
-const app = express();
+import {express, path, app, firebase, firebaseConfig} from 'config.js';
+
+await firebase.initializeApp(firebaseConfig);
+
 const PORT = process.env.PORT||80;
+
 app.get('/',(req, res)=>{
     res.end('<h1>server work</h1>')
-})
+});
 
 app.listen(PORT, ()=>{
 
-})
+});
