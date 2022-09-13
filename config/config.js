e56844b855955e3ac9cd8cbd4e40fbf1a6fc9bc3
artemfob <artemfob@gmail.com>
2022-09-13 20:20:01 +0300

    successfull config to run server

--- config/config.js
+++ config/config.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const path = require('path')
 const app = express();
-const firebase = require('firebase')
 const http = require('http')
+const firebase = require('firebase-admin')
+const firestore = require('firebase-admin/firestore')
 const firebaseConfig = {
     apiKey: "AIzaSyC8p-8UeASrWJaSCcfIJl4U5HrRF4fZxjk",
     authDomain: "photostopers.firebaseapp.com",
@@ -11,4 +12,11 @@ const firebaseConfig = {
     messagingSenderId: "68439398764",
     appId: "1:68439398764:web:3740d8f0a6c516190c3b49"
 };
-module.exports = {express, path, app, firebase, firebaseConfig};
+
+const { initializeApp, cert } = require('firebase-admin/app');
+
+const serviceAccount = require('../firebase-key.json')
+
+initializeApp({...firebaseConfig, credential: cert(serviceAccount)});
+
+exports.args = {express, path, app, firebaseConfig, http, firebase, firestore};
